0e24a77e2d64bd655e3025149a940a32568c9b18
SamehElkhyat <ssalih292@gmail.com>
2025-07-05 15:15:39 +0300

    rbuild

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
-import { Component } from "react";
 import "./App.css";
-import SignUp from "./SignUp/SignUp";
-import About from "./About/About";
+import React from "react";
 import NavBar from "./NavBar/NavBar";
-import Footer from "./Footer/Footer";
-import Start from "./Start/Start";
-import Layout from "./Layout/Layout";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import SignIn from "./SignIn/SignIn";
-import { AnimatePresence } from "framer-motion";
-let routers = createBrowserRouter([
-  {
-    path: "",
-    element: <Layout />,
-    children: [
-      { path: "NavBar", element: <NavBar /> },
+import { Outlet } from "react-router-dom";
 
-      { path: "Movies", element: <About /> },
+function App() {
 
-      { path: "SignUp", element: <SignUp /> },
-
-      { path: "Home", element: <About /> },
-
-      { index: "Movies", element: <Start /> },
-
-      { path: "Footer", element: <Footer /> },
-
-      { path: "Regestier", element: <SignIn /> },
-    ],
-  },
-]);
-export default class App extends Component {
-  render() {
-    return (
-      <AnimatePresence>
-        <RouterProvider router={routers}></RouterProvider>
-      </AnimatePresence>
-    );
-  }
+  return (
+    <>
+  
+      <NavBar />
+      <Outlet />
+    </>
+  );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,44 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import App from './App';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import SignUp from "./SignUp/SignUp";
+import About from "./About/About";
+import NavBar from "./NavBar/NavBar";
+import Footer from "./Footer/Footer";
+import Start from "./Start/Start";
+import Layout from "./Layout/Layout";
+import SignIn from "./SignIn/SignIn";
 import reportWebVitals from './reportWebVitals';
+
+let routers = createBrowserRouter([
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      { path: "NavBar", element: <NavBar /> },
+
+      { path: "Movies", element: <About /> },
+
+      { path: "SignUp", element: <SignUp /> },
+
+      { path: "Home", element: <About /> },
+
+      { index: "Movies", element: <Start /> },
+
+      { path: "Footer", element: <Footer /> },
+
+      { path: "Regestier", element: <SignIn /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App/>
-  // </React.StrictMode>
+  <AnimatePresence>
+    <RouterProvider router={routers}></RouterProvider>
+  </AnimatePresence>
 );
 
 reportWebVitals();
